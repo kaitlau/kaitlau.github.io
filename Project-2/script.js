721ace34e7e07db21903c26bf9c0da218e6f2e7f
kaitlau <157996681+kaitlau@users.noreply.github.com>
2024-04-03 15:59:56 -0400

    mm
    mm

--- Project-2/script.js
+++ Project-2/script.js
@@ -15,33 +15,45 @@ opacity.oninput = function() {
   opacityText.innerHTML = this.value;
 }
 
+
 //sketch 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+//function runCanvas() {
+  let canvasDiv = document.getElementById('sketch');
+  function setup() {
+    let width = canvasDiv.offsetWidth;
+    let height = width;
+   let runCanvas = createCanvas(width/1.53, height/1.53);
+runCanvas.parent("sketch");
   x = random(width);
   y = random(height);
-}
-
-function draw() {
-  //background("teal");
-  frameRate(10);
-  noStroke();
   
+  }
+
+  function draw() {
+  //background("teal");
+ s = frames.value;
+  frameRate(3);
+  noStroke();
+
  //SHAPE ATTRIBUTES 
-fill(random(["rgba(255,228,196,0.46)","rgba(0,128,128,0.34)","rgba(230,230,250,0.52)","rgba(255,0,0,0.34)", "rgba(255,234,72,0.49)"]));
-  //fill(0,100, 1, 50);
+  fill(random(["rgb(255,228,196,0.46)","rgba(0,128,128,0.34)","rgba(230,230,250,0.52)","rgba(255,0,0,0.34)", "rgba(255,234,72,0.49)"]));
   rect(random(0,700),random(0,700),random(50,700),random(50,700));
 
-  
  //LINE ATTRIBUTES
  let x2 = random(width);
  let y2 = random(height);
-stroke("gray");
+ stroke("black");
  line(x,y,x2,y2);
  x = x2;
  y = y2;
-  
 }
+function windowResized() {
+  let width = canvasDiv.offsetWidth;
+  let height = width;
+  resizeCanvas(width/1.53, height/1.53);}
+
+//}
+
 
 //music on image hover
 let audio = document.getElementById("audio");
