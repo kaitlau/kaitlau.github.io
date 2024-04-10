250f6e2449306c51eb285959ca70cdd0883b645f
kaitlau <157996681+kaitlau@users.noreply.github.com>
2024-04-09 21:44:27 -0400

    yippeee

---
+++ Project-2/draw.js
@@ -0,0 +1,32 @@
+let lineColor;
+
+function setup() {
+  createCanvas(1500, 650);
+
+  lineColor = color(0);
+
+}
+
+function draw() {
+  
+  stroke(lineColor);
+
+  //draw the rectangle button over the mouse trail
+  fill(lineColor);
+  rect(80, 20, 100, 90);
+
+  if (mouseIsPressed) {
+
+    line(pmouseX, pmouseY, mouseX, mouseY);
+    //if the mouse is over the rectangle
+    if ((mouseX > 80) && (mouseX < 80 + 100) &&
+      (mouseY > 20) && (mouseY < 20 + 90)) {
+
+      //set the line stroke to blue
+     lineColor = color(random(255),random(255), random(255));
+
+    }
+
+  }
+
+}
--- Project-2/script.js
+++ Project-2/script.js
@@ -5,6 +5,7 @@ frameText.innerHTML = frames.value;
 
 frames.oninput = function() {
   frameText.innerHTML = this.value;
+  frameR = this.value;
 }
 
 let opacity = document.getElementById("opacity-slider");
@@ -17,42 +18,55 @@ opacity.oninput = function() {
 
 
 //sketch 
-//function runCanvas() {
-  let canvasDiv = document.getElementById('sketch');
+let canvasDiv = document.getElementById("sketch");
+let frameR = 1; 
+let slider;
+let x, y;
+let width, height
+ 
+
   function setup() {
-    let width = canvasDiv.offsetWidth;
-    let height = width;
-   let runCanvas = createCanvas(width/1.53, height/1.53);
+    width = canvasDiv.offsetWidth;
+    height = width;
+   let runCanvas = createCanvas(width/1.28, height/1.5);
 runCanvas.parent("sketch");
+runCanvas.id("art")
   x = random(width);
   y = random(height);
-  
+
+
+
   }
 
   function draw() {
-  //background("teal");
- s = frames.value;
-  frameRate(3);
+
   noStroke();
 
  //SHAPE ATTRIBUTES 
-  fill(random(["rgb(255,228,196,0.46)","rgba(0,128,128,0.34)","rgba(230,230,250,0.52)","rgba(255,0,0,0.34)", "rgba(255,234,72,0.49)"]));
-  rect(random(0,700),random(0,700),random(50,700),random(50,700));
+  fill(random(["rgba(0,128,128,0.30)","rgba(230,230,250,0.3)","rgba(255,0,0,0.3)", "rgba(255,234,72,0.3)"]));
+  rect(random(0,700),random(0,700),random(300,1000),random(300,1000));
 
  //LINE ATTRIBUTES
+
  let x2 = random(width);
  let y2 = random(height);
  stroke("black");
  line(x,y,x2,y2);
  x = x2;
  y = y2;
-}
-function windowResized() {
-  let width = canvasDiv.offsetWidth;
-  let height = width;
-  resizeCanvas(width/1.53, height/1.53);}
 
-//}
+frameRate(int(frameR));
+}
+
+
+function windowResized() {
+  width = canvasDiv.offsetWidth;
+  height = width;
+  resizeCanvas(width/1.28, height/1.5);
+
+}
+
+
 
 
 //music on image hover
