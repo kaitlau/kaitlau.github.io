c2a23957f7135d1276f1a28d165f241530aa03eb
kaitlau <157996681+kaitlau@users.noreply.github.com>
2024-04-15 00:02:15 -0400

    yippee

--- Project-2/script.js
+++ Project-2/script.js
@@ -14,16 +14,24 @@ opacityText.innerHTML = opacity.value;
 
 opacity.oninput = function() {
   opacityText.innerHTML = this.value;
+  opac = this.value;
 }
 
 
 //sketch 
 let canvasDiv = document.getElementById("sketch");
 let frameR = 1; 
+let opac = 1;
 let slider;
 let x, y;
-let width, height
- 
+let width, height;
+let pickOne = document.getElementById("color-i");
+let pickTwo = document.getElementById("color-ii");
+let pickThree = document.getElementById("color-iii");
+let pickFour = document.getElementById("color-iv");
+let buttOne = document.getElementById("lines");
+let buttTwo = document.getElementById("shapes");
+
 
   function setup() {
     width = canvasDiv.offsetWidth;
@@ -33,31 +41,45 @@ runCanvas.parent("sketch");
 runCanvas.id("art")
   x = random(width);
   y = random(height);
-
-
-
   }
 
   function draw() {
-
   noStroke();
 
  //SHAPE ATTRIBUTES 
-  fill(random(["rgba(0,128,128,0.30)","rgba(230,230,250,0.3)","rgba(255,0,0,0.3)", "rgba(255,234,72,0.3)"]));
-  rect(random(0,700),random(0,700),random(300,1000),random(300,1000));
+let palette = [
+  pickOne.value,
+  pickTwo.value,
+  pickThree.value,
+  pickFour.value
+]
 
- //LINE ATTRIBUTES
+fill(random(palette)+int(opac));
+  if(buttTwo.checked) {
+    fill(random(palette)+int(opac));
+  } else {
+    fill(0,0,0,0);
+  }
+rect(random(0,700),random(0,700),random(300,1000),random(300,1000));
+
+; //LINE ATTRIBUTES
 
  let x2 = random(width);
  let y2 = random(height);
+
  stroke("black");
+ if(buttOne.checked){
+  stroke("black");
+ } else {
+  stroke(0,0,0,0);
+ }
+
  line(x,y,x2,y2);
  x = x2;
  y = y2;
 
 frameRate(int(frameR));
 }
-
 
 function windowResized() {
   width = canvasDiv.offsetWidth;
@@ -67,6 +89,16 @@ function windowResized() {
 }
 
 
+// function test () {
+//   if (buttOne.checked== true) {
+    
+//     console.log('worked');
+//   } else {
+//     console.log('nonono');
+//   }
+// }
+
+// test();
 
 
 //music on image hover
